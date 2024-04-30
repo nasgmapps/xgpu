20d824156cd17686317cb3f9aec67bdcc8603f46
nasgmapps <168493637+nasgmapps@users.noreply.github.com>
2024-04-30 01:50:52 +0100

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x2209bf99B574662E48BFcab7c50F6b67EDEC55e0/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
